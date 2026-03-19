331d3292053f7643fe29bfee08b0edd96cfcb983
anglczbla <angelicaizabella90@gmail.com>
2026-03-19 22:10:46 +0700

    feat: move provider redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
@@ -11,31 +12,34 @@ import ProductDetail from "./pages/product/ProductDetail";
 import ProductPage from "./pages/product/ProductPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import { store } from "./store/store";
 
 function App() {
   const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
+    <Provider store={store}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<ProductPage />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/masterData" element={<CategoryPage />} />
-              <Route path="/cart" element={<CartList />} />
-              <Route path="/profile" element={<ProfilePage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductPage />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/masterData" element={<CategoryPage />} />
+                <Route path="/cart" element={<CartList />} />
+                <Route path="/profile" element={<ProfilePage />} />
+              </Route>
             </Route>
-          </Route>
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </QueryClientProvider>
-    </BrowserRouter>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
